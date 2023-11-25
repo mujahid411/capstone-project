1e530b7484939902e451091e5f56d5abd7ada5a5
syedmujahidahmed <syedmujahidahmed0@gmail.com>
2023-11-26 04:34:34 +0530

    changes

--- client/src/components/notes.js
+++
@@ -1,4 +0,0 @@
-// phind.com
-// 
-// .populate(mongodb)
-// sdks web hooks
--- server/models/CourseModel.js
+++ server/models/CourseModel.js
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose';
 
 let courseSchema = new mongoose.Schema({
-    courseAuthor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Teacher',
-    },
+   
     courseTitle: {
         type: String,
         // required:true
